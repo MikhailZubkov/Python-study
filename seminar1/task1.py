877b578b0f187c53f3cd46d90c6ad9ab4664d08b
MikhailZubkov <toothkov@gmail.com>
2023-04-04 15:51:32 +0500

    1st seminar dz

---
+++ seminar1/task1.py
@@ -0,0 +1,7 @@
+#Задача 2: Найдите сумму цифр трехзначного числа.
+print('введите 3х значное число')
+number = str(input())
+a = int(number[0])
+b = int(number[1])
+c = int(number[2])
+print (a+b+c)
